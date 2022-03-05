f5be328740cd539081a2f67587b93dac28b35c61
Pankaj Bhagwat <Pankaj.Bhagwat@hotmail.com>
2022-03-05 08:52:36 +0530

    Add project files.

---
+++ doggydollar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace doggydollar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
